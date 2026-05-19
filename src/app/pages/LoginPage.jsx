c14c7908b5191d9614778f3f7128ca3480a04c7d
Dasun53 <dasununimail@gmail.com>
2026-05-19 08:09:03 +0530

    Added starting code for login page

--- src/app/pages/LoginPage.jsx
+++ src/app/pages/LoginPage.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router";
+import { Sparkles, Mail, Lock, Github, Chrome } from "lucide-react";
+
 export function LoginPage() {
   return <div className="min-h-screen p-8">Login Page</div>;
 }
