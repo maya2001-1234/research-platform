604e44508c5fe3479765795950df60debac4fde4
Dasun53 <dasununimail@gmail.com>
2026-05-20 22:26:04 +0530

    Adding import files to register page

--- src/app/pages/RegisterPage.jsx
+++ src/app/pages/RegisterPage.jsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router";
+import { Sparkles, Mail, Lock, Chrome } from "lucide-react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+
 export function RegisterPage() {
   return <div className="min-h-screen p-8">Register Page</div>;
 }
